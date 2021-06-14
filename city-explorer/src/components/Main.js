74292a129ad33292cd3469e58e45ec8dfefd7e6a
Samer Odeh <samerodeh220@gmail.com>
2021-06-14 21:10:27 +0300

    edit lab 06

--- city-explorer/src/components/Main.js
+++ city-explorer/src/components/Main.js
@@ -7,60 +7,87 @@ import './style.css';
 
 
 export class Main extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        cityExplorer: '',
-        cityInformation: {},
-        displayInformation: false
-      }
-    };
-  
-    changeCityExplorer = (event) => {
-      this.setState({
-        cityExplorer: event.target.value
-      });
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityExplorer: '',
+      cityInformation: {},
+      displayInformation: false,
+      error: '',
+      alert: false
     }
-  
-    getCityInformation = async (event) => {
-      event.preventDefault();
+  };
+
+  changeCityExplorer = (event) => {
+    this.setState({
+      cityExplorer: event.target.value
+    });
+  }
+
+
+  getCityInformation = async (event) => {
+    event.preventDefault();
+    try {
       const axiosResponse = await axios.get(`https://us1.locationiq.com/v1/search.php?key=pk.7c29bd71f87227f27ebcd9f2dbd2a2db&city=${this.state.cityExplorer}&format=json`);
-  
       console.log(axiosResponse);
       this.setState({
         cityInformation: axiosResponse.data[0],
-        displayInformation: true
+        displayInformation: true,
+        alert: false
       });
-    }
-  
-  
+    } catch (error) {
+      this.setState({
+        error: error.message,
+        alert: true
+      });
+    } 
+  }
+
+
     render() {
       return (
+      
+
         <div>
-  
+
+{this.state.alert &&
+
+<div class="alert alert-warning" role="alert">
+Warning : Wrong Input
+</div>
+}
+
           <Form onSubmit={this.getCityInformation}>
-  
+
             <Form.Group className="mb-3" controlId="formBasicEmail">
-  
+
               <Form.Label>City Name</Form.Label>
               <br></br>
               <input onChange={this.changeCityExplorer} type="text" />
-  
+
             </Form.Group>
-  
+
             <Button variant="primary" type="submit">Explorer</Button>
             <br></br>
+
             <p>{this.state.cityInformation.display_name}</p>
+            <p>{this.state.cityInformation.lat}</p>
+            <p>{this.state.cityInformation.lon}</p>
+
+            <br></br>
+
             {this.state.displayInformation &&
 
-            <img src={`https://maps.locationiq.com/v3/staticmap?key=pk.7c29bd71f87227f27ebcd9f2dbd2a2db&q&center=${this.state.cityInformation.lat},${this.state.cityInformation.lon}&zoom = 15`} alt='' />
+              <img src={`https://maps.locationiq.com/v3/staticmap?key=pk.7c29bd71f87227f27ebcd9f2dbd2a2db&q&center=${this.state.cityInformation.lat},${this.state.cityInformation.lon}&zoom = 15`} alt='' />
             }
+
           </Form>
-  
+
         </div>
-  
+
       )
     }
   }
 
   export default Main;
+
